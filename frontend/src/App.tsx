dd60268e0892d2014214ac45a3bae8218a1f6b59
Medo <xmedo117@proton.me>
2026-06-10 21:16:34 +0200

    mock strony głównej

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import AuthLayout from "../components/AuthLayout";
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <AuthLayout title="Mock strony głównej">
+      <p className="text-center m-6">Kiedyś tu będzie coś więcej</p>
+
+      <Button onClick={handleLogout}>Wyloguj się</Button>
+    </AuthLayout>
+  );
+}
